77c5092ae48a97cb9c2faa4ddfe7f4899451a87f
RenShaoning <pakeypay@foxmail.com>
2016-05-03 23:32:30 +0800

    18-31周末整理。

--- task18/script.js
+++ task18/script.js
@@ -1,13 +1,11 @@
-var numberQueue=new Array(1,2,3,4);
+var numberQueue=new Array(4);
 
 function UpdateBlocks()
 {
-  var html = "";
-  var model = "";
-  for (var i=0; i<numberQueue.length; i++)
-  {
-    model = "<div class='block'>"+numberQueue[i]+"</div>"; //这个单引号很重要
-    html += model;
+            var html = "";
+            for (var i=0; i<numberQueue.length; i++)
+            {
+              html += "<div class='block'>"+numberQueue[i]+"</div>"; //这个单引号很重要
   } 
   var blockchart = document.getElementById('blockchart');
   blockchart.innerHTML = html;
